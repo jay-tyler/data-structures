6037acb0bbbb7918281e0f79ce2ebb65185e903d
Jason Tyler <jmtyler@gmail.com>
2015-07-22 15:03:52 -0700

    repr for Node; working on test helper

--- bst.py
+++ bst.py
@@ -1,9 +1,13 @@
 class Node(object):
-        def __init__(self):
-            self.val = None
-            self.parent = None
-            self.left = None
-            self.right = None
+        def __init__(self, val, parent=None, left=None, right=None):
+            self.val = val
+            self.parent = parent
+            self.left = left
+            self.right = right
+
+        def __repr__(self):
+            return ("Node({self.val}, parent={self.parent},"\
+                    "left={self.left}, right={self.right})".format(self=self))
 
 
 class BST(object):
@@ -12,16 +16,20 @@ class BST(object):
 
     def insert(self, val):
         """Insert val into BST if not present. Always returns None."""
+        pass
 
     def contains(self, val):
         """Return True if val in BST. Else, return False."""
+        pass
 
     def size(self):
         """Return int size of tree. Will return 0 if tree is empty."""
+        pass
 
     def depth(self):
         """Return the int number of levels in the tree.
         Return 0 if tree is empty."""
+        pass
 
     def balance(self):
         """Return the difference between the number of nodes on the left with
@@ -29,6 +37,7 @@ class BST(object):
         --Negative if more on right
         --Positive if more on left
         --Zero if tree is perfectly balanced"""
+        pass
 
     def _find(self, root, val):
         """Return a tuple containing (node, side) with target val if it exists,
@@ -46,3 +55,9 @@ class BST(object):
 
         if self.root is not None:
             return _look(self.root)
+
+
+def test_helper():
+    A = Node(5)
+    B = Node(4, parent=A)
+    C = Node(6, parent=)
