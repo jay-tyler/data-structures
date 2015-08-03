3ad9db6b997b39467d4c11bb27c12333c2b5fab4
Jason Tyler <jmtyler@gmail.com>
2015-08-03 15:32:20 -0700

    added test block for best/worst cases

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,4 +1,4 @@
- def insort(slist):
+def insort(slist):
     """Insertion sort a list
 
     Implementation follows after gif here
@@ -14,3 +14,29 @@
             else:
                 break 
     return slist
+
+
+if __name__ == "__main__":
+    """Test time performance for best and worst cases"""
+    import time
+    # time is better than timeit for unix times
+    # https://docs.python.org/2/library/timeit.html#timeit.default_timer
+
+    # O(n) best case
+    start = time.time()
+    for i in range(100):
+        insort(range(100))
+    stop = time.time()
+    best_time = (stop - start) / 100
+
+    #O(n**2) worst case 
+    start = time.time()
+    for i in range(100):
+        insort(range(100)[::-1])
+    stop = time.time()
+    worst_time = (stop - start) / 100
+
+    print "best case: {best}\n\nworst case: {worst}".format(
+        best=best_time, worst=worst_time) +\
+        "\n\nBest case is {} times better than worst for n=100".format(
+        worst_time/best_time)
