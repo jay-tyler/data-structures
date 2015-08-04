6c654bcd237186ff3e1f2a42fe44220f6296ca0b
Jason Tyler <jmtyler@gmail.com>
2015-08-04 14:39:37 -0700

    mersort added

--- merge_sort.py
+++ merge_sort.py
@@ -1,5 +1,9 @@
-
 def merge(llist, rlist):
+    """Return an ascending sorted list from two ascending sorted lists.
+
+    Implementation after psuedocode here:
+    https://en.wikipedia.org/wiki/Merge_sort
+    """
     mlist = []
     llist = llist[:]
     rlist = rlist[:]
@@ -16,3 +20,21 @@ def merge(llist, rlist):
     while rlist:
         mlist.append(rlist.pop())
     return mlist
+
+
+def mersort(unslist):
+    """Return a sorted list from a list
+
+    Implementation after psuedocode here:
+    https://en.wikipedia.org/wiki/Merge_sort
+    """
+    if len(unslist) <= 1:
+        return unslist
+    lunslist = unslist[:len(unslist)/2]
+    runslist = unslist[len(unslist)/2:]
+
+    # Recursive calls
+    lunslist = mersort(lunslist)
+    runslist = mersort(runslist)
+
+    return merge(lunslist, runslist)
